2d42de64db6c217b91aea499785ab07eaf65938e
Dave P <itsmedavep@gmail.com>
2019-03-09 22:47:17 -0600

    Update RuntimeFontMac.cpp

--- src/mac/RuntimeFontMac.cpp
+++ src/mac/RuntimeFontMac.cpp
@@ -42,7 +42,7 @@ void initializeRuntimeFont()
     displayFont = minifont;
     patchNameFont = patchfont;
     lfoTypeFont = lfofont;
-    aboutFSont = aboutfont;
+    aboutFont = aboutfont;
 }
 
 }
